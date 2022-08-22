ec4236d12efc7655fd519276217e97d7b1afba85
Sytac Dev <31990977+sytac-bot@users.noreply.github.com>
2022-08-22 15:35:36 +0200

    Initial devcase commit

---
+++ src/main/java/org/sytac/test/implement/tennis/TennisImpl.java
@@ -0,0 +1,19 @@
+package org.sytac.test.implement.tennis;
+
+import com.google.auto.service.AutoService;
+import org.sytac.test.internal.Tennis;
+
+import java.util.List;
+
+@AutoService(Tennis.class)
+public class TennisImpl implements Tennis {
+
+    /**
+     * Implement this method, please add as many helper methods or classes as you need
+     * @param points a list of players who scored each point, eg. "P1,P2,P2,P2,P1"
+     */
+    @Override
+    public String process(List<String> points) {
+        return "sets: 2-1 games: 1-1 points: 40-15";
+    }
+}
